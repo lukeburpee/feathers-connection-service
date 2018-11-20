c5adf8e37f9dd83005ef8e5e4f3df2a460308237
lukeburpee <lukeburpee@gmail.com>
2018-11-20 16:34:33 -0500

    Updating changelog

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,6 @@ import ConnectionService from './connection-service'
 
 export default ConnectionService
 
+export { Service as ConnectionClass } from './connection-service'
+
 export { default as BaseService } from './base-service'
